c037532b0002a996eb052c51ebaa8ac71f278c6f
Mara's Mac <williamsmc@my.lanecc.edu>
2024-02-04 14:38:48 -0800

    midterm files without node modules folder

---
+++ midterm/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
+
+
+const HomeScreen = ({navigation}) => {
+
+  return <View>
+    <Text style={styles.text}>This is a view.</Text>
+  </View>
+
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 30,
+  },
+});
+
+export default HomeScreen;
